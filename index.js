8c67b7d4b09b1d7f53d831669e54295d047ce18a
wenyin <342306219@qq.com>
2019-12-06 16:54:14 +0800

    fix

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+alert(666)
 if ('serviceWorker' in window.navigator) {
+    alert(555)
     navigator.serviceWorker.register('./sw.js', {scope: './'})
     
     navigator.serviceWorker.addEventListener('controllerchange', () => {
